d80ba49ad803d29ebec7f3ae42805e38151b1316
Fatih Kaya <fthky35@gmail.com>
2026-01-24 00:55:14 +0300

    feat: forward ping from gateway to api with X-Request-Id

--- Lab.Gateway/Program.cs
+++ Lab.Gateway/Program.cs
@@ -1,6 +1,32 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddHttpClient("api", (serviceProvider, client) =>
+{
+    var configuration = serviceProvider.GetRequiredService<IConfiguration>();
+    client.BaseAddress = new Uri(configuration["Api:BaseUrl"]!);
+});
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/ping", async (HttpContext context, IHttpClientFactory httpClientFactory) =>
+{
+    var correlationId = context.Request.Headers["X-Request-Id"].FirstOrDefault();
+    if (string.IsNullOrWhiteSpace(correlationId))
+    {
+        correlationId = Guid.NewGuid().ToString("N");
+    }
+    
+    context.Response.Headers["X-Request-Id"] = correlationId;
+
+    var client = httpClientFactory.CreateClient("api");
+
+    using var req = new HttpRequestMessage(HttpMethod.Get, "/ping");
+    req.Headers.TryAddWithoutValidation("X-Request-Id", correlationId);
+
+    var res = await client.SendAsync(req);
+
+    var body = await res.Content.ReadAsStringAsync();
+
+    return Results.Content(body, "text/plain");
+});
 
 app.Run();
